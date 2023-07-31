c8f50c3a7a865250000c45a545ecaf0cc0a5ccb8
kwa_laa <khoaddnguyen@gmail.com>
2023-07-31 18:50:50 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,31 @@
+#Call clear() to clear the output in the console.
+from replit import clear
+from art import logo
+
+print(logo)
+
+bids = {}
+bidding_finished = False
+
+
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+
+while not bidding_finished:
+    name = input("Please insert your name here: ")
+    price = int(input("How much would you like to bid? $"))
+    #Declare dictionary "bids", key "name", value "price"
+    bids[name] = price
+    should_continue = input("Are there any other bidders? Type 'yes' or 'no'.")
+    if should_continue == 'no':
+        bidding_finished = True
+        find_highest_bidder(bids)
+    elif should_continue == 'yes':
+        clear()
